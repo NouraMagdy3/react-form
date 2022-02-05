ef08d30e4ce29ce7c54873deabf0b284848300cb
Noura Magdy <magdynoura095@gmail.com>
2022-02-05 13:44:15 +0200

    secondCommit

--- src/pages/FormThree/FormThree.jsx
+++ src/pages/FormThree/FormThree.jsx
@@ -44,10 +44,10 @@ const residences = [
 ];
 
 const FormThree = () => {
-      const [dataform, setDataform] = React.useState(true);
+    const [dataform, setDataform] = React.useState(true);
 
     // hooks
-    const { control, watch, handleSubmit,setValue, formState: { errors } } = useForm(
+    const { control, watch, handleSubmit, setValue, formState: { errors } } = useForm(
         {
             resolver: yupResolver(registerAccountSchema),
             defaultValues: {
@@ -64,29 +64,15 @@ const FormThree = () => {
         setDataform(false);
     }
 
-     //useEffect
-     React.useEffect(() => {
-        /*const formSaved = localStorage.getItem("formData");
-        /*const emailVal = JSON.parse(formSaved).email;
-        const passwordVal = JSON.parse(formSaved).password;
-        const nickNameVal = JSON.parse(formSaved).nickName;
-        const habitualVal = JSON.parse(formSaved).habitualResidence;
-        const phoneNumberVal = JSON.parse(formSaved).phoneNumber;
-        setValue('email', emailVal);
-        setValue('password', passwordVal);
-        setValue('nickName', nickNameVal);
-        setValue('habitualResidence', habitualVal);
-        setValue('phoneNumber', phoneNumberVal);*
-         for (const property in formSaved) {
-        setValue(property, formSaved[property]);
-      }*/
-      if (localStorage.getItem("formData")) {
-      const formData = JSON.parse(localStorage.getItem("formData"));
-      for (const property in formData) {
-        setValue(property, formData[property]);
-      }
-      }
-      }, []);
+    //useEffect
+    React.useEffect(() => {
+        if (localStorage.getItem("formData")) {
+            const formData = JSON.parse(localStorage.getItem("formData"));
+            for (const property in formData) {
+                setValue(property, formData[property]);
+            }
+        }
+    }, [setValue]);
 
     const { agreement } = watch();
 
@@ -107,132 +93,132 @@ const FormThree = () => {
     return (
         <div className="container">
             <Title>Register you account </Title>
-             {dataform ? (
-            <Form className='form' onSubmitCapture={handleSubmit(submit)}>
-                <div className='input-cell' >
-                    <Text className='label'>Email :</Text>
-                    <div className='input-content'>
+            {dataform ? (
+                <Form className='form' onSubmitCapture={handleSubmit(submit)}>
+                    <div className='input-cell' >
+                        <Text className='label'>Email :</Text>
+                        <div className='input-content'>
 
-                        <Controller
-                            name="email"
-                            control={control}
-                            render={({ field }) => (
-                                <Input
-                                    placeholder='email'
-                                    type="email"
-                                    {...field}
-                                    
-                                />
-                            )}
-                        />
-                        {errors.email && <Text className='error'>{errors.email.message}</Text>}
-                    </div>
-                </div>
-                <div className='input-cell' >
-                    <Text className='label'>Password :</Text>
-                    <div className='input-content'>
-                        <Controller
-                            name="password"
-                            control={control}
-                            render={({ field }) => (
-                                <Input.Password
-                                    placeholder='password'
-                                    type={"password"}
-                                    {...field}
-                                />
-                            )}
-                        />
-                        {errors.password && <Text className='error'>{errors.password.message}</Text>}
-                    </div>
-                </div>
-                <div className='input-cell' >
-                    <Text className='label'>Confirm Password :</Text>
-                    <div className='input-content'>
-                        <Controller
-                            name="confirmPassword"
-                            control={control}
-                            render={({ field }) => (
-                                <Input.Password
-                                    placeholder='confirm password'
-                                    type={"password"}
-                                    {...field}
-                                />
-                            )}
-                        />
-                        {errors.confirmPassword && <Text className='error'>{errors.confirmPassword.message}</Text>}
-                    </div>
-                </div>
-                <div className='input-cell' >
-                    <Text className='label'>Nick name :</Text>
-                    <div className='input-content'>
-                        <Controller
-                            name="nickName"
-                            control={control}
-                            render={({ field }) => (
-                                <Input
-                                    placeholder='eg: linda'
-                                    {...field}
-                                />
-                            )}
-                        />
-                        {errors.nickName && <Text className='error'>{errors.nickName.message}</Text>}
-                    </div>
-                </div>
-                <div className='input-cell' >
-                    <Text className='label'>Habitual Residence" :</Text>
-                    <div className='input-content-r'>
-                        <Controller
-                            name="habitualResidence"
-                            control={control}
-                            render={({ field }) => (
-                                <Cascader
-                                    options={residences}
-                                    {...field}
-                                />
-                            )}
-                        />
-                        {errors.habitualResidence && <Text className='error'>{errors.habitualResidence.message}</Text>}
-                    </div>
-                </div>
-                <div className='input-cell' >
-                    <Text className='label'>Phone Number :</Text>
-                    <div className='input-content'>
-                        <Controller
-                            name="phoneNumber"
-                            control={control}
-                            render={({ field }) => (
-                                <Input
-                                    placeholder='50888888'
-                                    addonBefore={prefixSelector}
-                                    {...field}
-                                />
-                            )}
-                        />
-                        {errors.phoneNumber && <Text className='error'>{errors.phoneNumber.message}</Text>}
-                    </div>
+                            <Controller
+                                name="email"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        placeholder='email'
+                                        type="email"
+                                        {...field}
 
-                </div>
-                <div className='input-cell' >
-                    <div className='input-content'>
-                        <Controller
-                            name="agreement"
-                            control={control}
-                            render={({ field }) => (
-                                <Checkbox {...field}>
-                                    I have read the <a href="">agreement</a>
-                                </Checkbox>
-                            )}
-                        />
-                        {errors.agreement && <Text className='error'>{errors.agreement.message}</Text>}
+                                    />
+                                )}
+                            />
+                            {errors.email && <Text className='error'>{errors.email.message}</Text>}
+                        </div>
                     </div>
-                </div>
-                <Button disabled={!agreement || Object.keys(errors).length > 0} className='submit' type="primary" htmlType='submit'>
-                    Submit
-                </Button>
-            </Form>
+                    <div className='input-cell' >
+                        <Text className='label'>Password :</Text>
+                        <div className='input-content'>
+                            <Controller
+                                name="password"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input.Password
+                                        placeholder='password'
+                                        type={"password"}
+                                        {...field}
+                                    />
+                                )}
+                            />
+                            {errors.password && <Text className='error'>{errors.password.message}</Text>}
+                        </div>
+                    </div>
+                    <div className='input-cell' >
+                        <Text className='label'>Confirm Password :</Text>
+                        <div className='input-content'>
+                            <Controller
+                                name="confirmPassword"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input.Password
+                                        placeholder='confirm password'
+                                        type={"password"}
+                                        {...field}
+                                    />
+                                )}
+                            />
+                            {errors.confirmPassword && <Text className='error'>{errors.confirmPassword.message}</Text>}
+                        </div>
+                    </div>
+                    <div className='input-cell' >
+                        <Text className='label'>Nick name :</Text>
+                        <div className='input-content'>
+                            <Controller
+                                name="nickName"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        placeholder='eg: linda'
+                                        {...field}
+                                    />
+                                )}
+                            />
+                            {errors.nickName && <Text className='error'>{errors.nickName.message}</Text>}
+                        </div>
+                    </div>
+                    <div className='input-cell' >
+                        <Text className='label'>Habitual Residence" :</Text>
+                        <div className='input-content-r'>
+                            <Controller
+                                name="habitualResidence"
+                                control={control}
+                                render={({ field }) => (
+                                    <Cascader
+                                        options={residences}
+                                        {...field}
+                                    />
+                                )}
+                            />
+                            {errors.habitualResidence && <Text className='error'>{errors.habitualResidence.message}</Text>}
+                        </div>
+                    </div>
+                    <div className='input-cell' >
+                        <Text className='label'>Phone Number :</Text>
+                        <div className='input-content'>
+                            <Controller
+                                name="phoneNumber"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        placeholder='50888888'
+                                        addonBefore={prefixSelector}
+                                        {...field}
+                                    />
+                                )}
+                            />
+                            {errors.phoneNumber && <Text className='error'>{errors.phoneNumber.message}</Text>}
+                        </div>
+
+                    </div>
+                    <div className='input-cell' >
+                        <div className='input-content'>
+                            <Controller
+                                name="agreement"
+                                control={control}
+                                render={({ field }) => (
+                                    <Checkbox {...field}>
+                                        I have read the <a href="">agreement</a>
+                                    </Checkbox>
+                                )}
+                            />
+                            {errors.agreement && <Text className='error'>{errors.agreement.message}</Text>}
+                        </div>
+                    </div>
+                    <Button disabled={!agreement || Object.keys(errors).length > 0} className='submit' type="primary" htmlType='submit'>
+                        Submit
+                    </Button>
+                </Form>
             ) : (
-        <div></div>
-      )}
+                <div></div>
+            )}
         </div>
     )
 }
